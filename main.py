eb650390fc6e719f484377f6c257bb87cbf30917
genfu94 <giuseppe.gelfusa@hotmail.it>
2022-10-28 17:40:48 +0200

    ADD data_ingestion: implemented method to download from GDrive

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import cv2
-import imutils
-from src.face_recognition.face_alignment import FaceAligner
-from src.face_recognition.face_detection import Dlib68LandmarksFaceDetector
+import src.data_ingestion as data_ingestion
 
-
-image = cv2.imread('test_image.jpg')
-image = imutils.resize(image, width=600)
-fa = FaceAligner(Dlib68LandmarksFaceDetector(), desiredFaceWidth=256, desiredLeftEye=(0.3, 0.3))
-aligned = fa.align(image)
-cv2.imshow("ASD", aligned)
-cv2.waitKey(0)
+data_ingestion.retrieve_file_from_url("http://www.kinfacew.com/dataset/KinFaceW-I.zip", "data/original/KinFaceW-I.zip")
+data_ingestion.retrieve_file_from_url("http://www.kinfacew.com/dataset/KinFaceW-I.zip", "data/original/KinFaceW-II.zip")
+data_ingestion.retrieve_file_from_url("http://www1.ece.neu.edu/~yunfu/research/Kinface/KinFace_V2.zip", "data/original/KinFace_V2.zip")
+data_ingestion.retrieve_file_from_google_drive("https://drive.google.com/file/d/1qhxX4YrJRXvxrybxQo0yGhkwUdmen-js/view", 'data/original/TSKinFace_Data.zip')
+data_ingestion.retrieve_file_from_url("http://chenlab.ece.cornell.edu/projects/KinshipClassification/Family101_150x120.zip", 'data/original/Family101_150x120.zip')
+data_ingestion.retrieve_file_from_url('http://chenlab.ece.cornell.edu/projects/KinshipVerification/KinshipVerification.zip', 'data/original/KinshipVerification.zip')
