1e185cf2cafce138f4d40f1310a0309418be815a
genfu94 <giuseppe.gelfusa@hotmail.it>
2022-10-29 08:05:24 +0200

    REFACTORING: data ingestion

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
+import os
+import shutil
 import src.data_ingestion as data_ingestion
 
-data_ingestion.retrieve_file_from_url("http://www.kinfacew.com/dataset/KinFaceW-I.zip", "data/original/KinFaceW-I.zip")
-data_ingestion.retrieve_file_from_url("http://www.kinfacew.com/dataset/KinFaceW-I.zip", "data/original/KinFaceW-II.zip")
-data_ingestion.retrieve_file_from_url("http://www1.ece.neu.edu/~yunfu/research/Kinface/KinFace_V2.zip", "data/original/KinFace_V2.zip")
-data_ingestion.retrieve_file_from_google_drive("https://drive.google.com/file/d/1qhxX4YrJRXvxrybxQo0yGhkwUdmen-js/view", 'data/original/TSKinFace_Data.zip')
-data_ingestion.retrieve_file_from_url("http://chenlab.ece.cornell.edu/projects/KinshipClassification/Family101_150x120.zip", 'data/original/Family101_150x120.zip')
-data_ingestion.retrieve_file_from_url('http://chenlab.ece.cornell.edu/projects/KinshipVerification/KinshipVerification.zip', 'data/original/KinshipVerification.zip')
+DATA_SOURCES = [
+    {
+        'url': "http://www.kinfacew.com/dataset/KinFaceW-I.zip",
+        'filepath': "data/original/KinFaceW-I.zip",
+        'source': data_ingestion.DownloadSource.HTTP
+    },
+    {
+        'url': "http://www.kinfacew.com/dataset/KinFaceW-II.zip",
+        'filepath': "data/original/KinFaceW-II.zip",
+        'source': data_ingestion.DownloadSource.HTTP
+    },
+    {
+        'url': "http://www1.ece.neu.edu/~yunfu/research/Kinface/KinFace_V2.zip",
+        'filepath': "data/original/KinFace_V2.zip",
+        'source': data_ingestion.DownloadSource.HTTP
+    },
+    {
+        'url': "https://drive.google.com/file/d/1qhxX4YrJRXvxrybxQo0yGhkwUdmen-js/view",
+        'filepath': "data/original/TSKinFace_Data.zip",
+        'source': data_ingestion.DownloadSource.GOOGLE_DRIVE
+    },
+    {
+        'url': "http://chenlab.ece.cornell.edu/projects/KinshipClassification/Family101_150x120.zip",
+        'filepath': "data/original/Family101_150x120.zip",
+        'source': data_ingestion.DownloadSource.HTTP
+    },
+    {
+        'url': "http://chenlab.ece.cornell.edu/projects/KinshipVerification/KinshipVerification.zip",
+        'filepath': "data/original/KinshipVerification.zip",
+        'source': data_ingestion.DownloadSource.HTTP
+    }
+]
+
+
+for data_source in DATA_SOURCES:
+    data_ingestion.retrieve_data_from_url(**data_source)
--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,9 +1,16 @@
+from enum import Enum
 from tqdm import tqdm
 import requests
 import gdown
+import os
 
 
-def retrieve_file_from_url(url: str, filepath: str, show_progres: bool = True):
+class DownloadSource(Enum):
+	HTTP = 1
+	GOOGLE_DRIVE = 2
+
+# TODO: make interfaces for these download methods!
+def _retrieve_file_from_url(url: str, filepath: str, show_progres: bool = True):
 	with requests.get(url, stream=True) as r:
 		r.raise_for_status()
 		with open(filepath, 'wb') as f:
@@ -15,5 +22,22 @@ def retrieve_file_from_url(url: str, filepath: str, show_progres: bool = True):
 
 
 # TODO: check why this takes a lot of time before start downloading
-def retrieve_file_from_google_drive(url: str, filepath: str, show_progress: bool = True):
+def _retrieve_file_from_google_drive(url: str, filepath: str, show_progress: bool = True):
 	gdown.download(url, filepath, quiet=False, fuzzy=show_progress)
+
+downloader_methods = {
+	DownloadSource.HTTP: _retrieve_file_from_url,
+	DownloadSource.GOOGLE_DRIVE: _retrieve_file_from_google_drive
+}
+
+def retrieve_data_from_url(url: str, filepath: str, source: DownloadSource, show_progress: bool = True):
+	if source not in downloader_methods:
+		raise("Download from the specified source is not implemented yet.")
+
+	downloader = downloader_methods[source]
+
+	if os.path.exists(filepath):
+		print(f"The resource {filepath} already exists. Skipping download.")
+		return
+
+	downloader(url, filepath, show_progress)
